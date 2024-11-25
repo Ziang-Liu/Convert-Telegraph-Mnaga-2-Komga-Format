149e45859ffb4111f4135e5790ab5816be946a6b
dariNirvana <takina0721@gmail.com>
2024-11-25 18:14:22 +0800

    :sparkles: 添加新功能模块

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
 from .api import ChatAnywhereApi, DmzjApi, TraceMoeApi
-from .service import AggregationSearch, Telegraph
+from .service import AggregationSearch, Telegraph, TelegraphDatabase
 from .utils import logger, proxy_init
 from .Environment import EnvironmentReader
--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 from .Search import AggregationSearch
-from .Telegraph import Telegraph
+from .Telegraph import Telegraph, TelegraphDatabase
